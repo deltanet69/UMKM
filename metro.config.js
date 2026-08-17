56982a78181711f5a915026f7f3d8db7f91b61c9
nfl1201 <naufalhamidy@gmail.com>
2026-08-18 06:16:53 +0700

    update action github

--- metro.config.js
+++ metro.config.js
@@ -1,6 +1,10 @@
 const { getDefaultConfig } = require("expo/metro-config");
 const { withNativeWind } = require("nativewind/metro");
+const path = require("path");
 
 const config = getDefaultConfig(__dirname);
+
+// Fix: Ensure EXPO_ROUTER_APP_ROOT is resolved at build time for web export
+process.env.EXPO_ROUTER_APP_ROOT = path.join(__dirname, "app");
 
 module.exports = withNativeWind(config, { input: "./global.css" });
